01e3ebb1a950669fac3f74d7b02bbd8bac467c08
Carlos <ordazhoyos2001@gmail.com>
2022-12-05 18:55:18 -0500

    photo

--- src/main/java/pe/com/midocvirtual/backend/repositories/OrdenRepository.java
+++ src/main/java/pe/com/midocvirtual/backend/repositories/OrdenRepository.java
@@ -13,8 +13,6 @@ public interface OrdenRepository extends JpaRepository<Orden, Long> {
     @Query(value = "select sum(o.total) as total,count(o.id) as cantidad from ordenes o \n" +
             "where o.farmacia_id=?1 and o.fecha>=?2 and o.fecha<=?3",nativeQuery = true)
     Object IngresosEntreFechas(Long idFarmacia,Date inicio,Date fin);
-    @Query("select o from Orden o where datediff(now(),o.fecha)<=3 and o.farmacia.id=?1 order by o.fecha desc")
-    List<Orden> findAllUltimos3Dias(Long idFarmacia);
     @Query( "select o.fecha as fecha,sum(o.total) as total from Orden o \n" +
             "where o.farmacia.id=?1 and o.fecha>=?2 and o.fecha<=?3 group by o.fecha order by o.fecha asc")
     List<Object> getOrdenesReporteSemanal(Long idFarmacia, Date inicio, Date fin);
